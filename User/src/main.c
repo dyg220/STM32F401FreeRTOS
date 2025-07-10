5bd64c51a6e7729abad0317ae6aa6f810f748718
dyg220 <1503639251@qq.com>
2025-07-10 14:15:47 +0800

    add

--- User/src/main.c
+++ User/src/main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 TaskHandle_t Task1_Handle_t;  // 任务1句柄
+TaskHandle_t Task2_Handle_t;  // 任务2句柄
 void Task1(void* pvParameters); // 任务1函数
-
+void Task2(void* pvParameters); // 任务1函数
 int main()
 {
 	BaseType_t ret;
@@ -10,7 +11,8 @@ int main()
 	USART1_Config(115200);
 	printf("Reset!\r\n");
 
-	ret = xTaskCreate(Task1, "Task1", 128, NULL, 3, &Task1_Handle_t); //任务创建出来让任务到就绪态
+	ret = xTaskCreate(Task1, "Task1", 128, NULL, 4, &Task1_Handle_t); //任务创建出来让任务到就绪态
+	ret = xTaskCreate(Task2, "Task2", 128, NULL, 3, &Task2_Handle_t); //任务创建出来让任务到就绪态
 	vTaskStartScheduler(); //开启任务调度 
 
 	return 0;
@@ -21,5 +23,16 @@ void Task1(void* pvParameters)
 	{
 		printf("Task1 Run \r\n");
 		vTaskDelay(500);
+
 	}
 }
+void Task2(void* pvParameters)
+{
+	while (1)
+	{
+		printf("Task2 Run \r\n");
+		vTaskDelay(500);
+
+	}
+}
+

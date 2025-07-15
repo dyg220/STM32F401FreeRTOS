0271e0f28cc509e5866b7a04cc09371a8a9e755f
dyg220 <1503639251@qq.com>
2025-07-15 19:31:18 +0800

    add

--- User/src/iic.c
+++ User/src/iic.c
@@ -3,23 +3,23 @@
 void IIC3_Init(void)
 {
 	//RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); // SCL:PB6, SDA:PB7
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+	//RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
 
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-	//GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_OD; // 复用开漏
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStruct);
+	//GPIO_InitTypeDef GPIO_InitStruct;
+	//GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+	////GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_OD; // 复用开漏
+	//GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	//GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	I2C_InitTypeDef I2C_InitStruct;
-	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
-	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;       // 50%占空比
-	I2C_InitStruct.I2C_OwnAddress1 = 0x00;                // 主机地址（可忽略）
-	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;              // 使能ACK
-	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-	I2C_InitStruct.I2C_ClockSpeed = 100000;               // 100kHz
-	I2C_Init(I2C1, &I2C_InitStruct);
-	I2C_Cmd(I2C1, ENABLE);                                // 使能I2C
+	//I2C_InitTypeDef I2C_InitStruct;
+	//I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
+	//I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;       // 50%占空比
+	//I2C_InitStruct.I2C_OwnAddress1 = 0x00;                // 主机地址（可忽略）
+	//I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;              // 使能ACK
+	//I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+	//I2C_InitStruct.I2C_ClockSpeed = 100000;               // 100kHz
+	//I2C_Init(I2C1, &I2C_InitStruct);
+	//I2C_Cmd(I2C1, ENABLE);                                // 使能I2C
 
 
 
--- User/src/main.c
+++ User/src/main.c
@@ -5,7 +5,6 @@ int main()
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); // 设置中断优先级分组
 	delay_init(84);
 	USART1_Config(115200);
-
 	LED_Init();
 	Key_Init();
 	Door_Init();
@@ -13,7 +12,6 @@ int main()
 	Voice_Init();
 	Voice_SendCmd(0xef);
 	RFID_Init();
-
 
 	printf("Reset!\r\n");
 	AppStartTask();
